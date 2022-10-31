7b6299ebfda8679f1f61b606787622c9de312376
deep <deep.inexture@gmail.com>
2022-10-31 11:46:44 +0530

    commented whole file as it has no further use of it.

--- demo_app/database.py
+++ demo_app/database.py
@@ -1,31 +1,31 @@
-from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
-import os
-
-"""
-This Page makes Database Connection to execute and close database connection after every use.
-The get_db function is called every time db calls.
-"""
-
-# Below function will load the required Environment variable to use anywhere in particular file.
-load_dotenv()
-
-# Connecting to Database using env variable which stores database URL.
-engine = create_engine(os.environ.get('LOCAL_DATABASE_URL'), echo=True)
-
-Base = declarative_base()
-SessionLocal = sessionmaker(bind=engine)
-
-
-# Following function can be called everytime to initialize db variable and use while firing query.
-def get_db():
-    """
-    This Method provides variable db which has access to all models section column names.
-    Thus, it passes query to database and acts as its object.
-    """
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# from sqlalchemy.orm import declarative_base, sessionmaker
+# from sqlalchemy import create_engine
+# from dotenv import load_dotenv
+# import os
+#
+# """
+# This Page makes Database Connection to execute and close database connection after every use.
+# The get_db function is called every time db calls.
+# """
+#
+# # Below function will load the required Environment variable to use anywhere in particular file.
+# load_dotenv()
+#
+# # Connecting to Database using env variable which stores database URL.
+# engine = create_engine(os.environ.get('LOCAL_DATABASE_URL'), echo=True)
+#
+# Base = declarative_base()
+# SessionLocal = sessionmaker(bind=engine)
+#
+#
+# # Following function can be called everytime to initialize db variable and use while firing query.
+# def get_db():
+#     """
+#     This Method provides variable db which has access to all models section column names.
+#     Thus, it passes query to database and acts as its object.
+#     """
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
