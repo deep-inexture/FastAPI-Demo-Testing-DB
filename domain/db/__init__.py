c0e2d793831e5fb05b1e916718c236f0c0c637f5
deep <deep.inexture@gmail.com>
2022-10-31 11:45:42 +0530

    updated Database_url to local db to check db instance changes or not.

--- domain/db/__init__.py
+++ domain/db/__init__.py
@@ -2,15 +2,17 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, scoped_session
+from dotenv import load_dotenv
 
-from demo_app.database import Base
+# from demo_app.database import Base
+load_dotenv()
 
 
-SQLALCHEMY_DATABASE_URL = os.environ.get('TESTING_DATABASE_URL')
+SQLALCHEMY_DATABASE_URL = os.environ.get('LOCAL_DATABASE_URL')
 engine: Engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
-# Base = declarative_base()
+Base = declarative_base()
 DBSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
